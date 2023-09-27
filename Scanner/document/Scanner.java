0961bbce3fe28403baddc822b5521fe8d5e03440
Zhao Liu <liu.9553@osu.edu>
2023-09-27 03:05:33 -0400

    initial document

---
+++ Scanner/document/Scanner.java
@@ -0,0 +1,28 @@
+class Scanner {
+
+    // Initialize the scanner
+    Scanner(String filename) {
+
+    }
+
+    // Advance to the next token
+    public void nextToken() {
+
+    }
+
+    // Return the current token
+    public Core currentToken() {
+
+    }
+
+	// Return the identifier string
+    public String getId() {
+
+    }
+
+	// Return the constant value
+    public int getConst() {
+
+    }
+
+}
